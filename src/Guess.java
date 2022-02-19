da8a03f5acf7d7305965a8a93195139518846a3b
OG Garrison <garrisonsv1@gmail.com>
2022-02-19 17:23:28 -0500

    name fix and addition of text file etc.

---
+++ src/Guess.java
@@ -0,0 +1,64 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Random;
+
+public class Guess {
+	private String word;
+	private String user;
+
+public String takeWord() {
+	try {
+	File dict = new File("src/words.txt");
+	Scanner scan = new Scanner(dict);
+	String[] retroWords = new String[6];
+	int i = 0;
+	while (scan.hasNext()) {
+		retroWords[i] = scan.next();
+		i++;
+	}
+	Random rand = new Random();
+	int number = rand.nextInt(6);
+	word = retroWords[number];
+	return word;
+	} catch (FileNotFoundException e) {
+		System.err.println(e.getMessage());
+	}
+	return "";
+}
+
+public void promptUser() {
+	Scanner stdIn = new Scanner(System.in);
+	System.out.print("Your Guess: ");
+	user = stdIn.nextLine();
+}
+public String compareWords() {
+	String[] temp = new String[5];
+	for (int i = 0 ; i < word.length(); i++) {
+		
+		for (int j = 0 ; j < user.length(); j++) {
+			
+			if(user.charAt(j) != word.charAt(i) && i == j){
+				temp[j] = "_";
+			}
+			
+			if (user.charAt(j) == word.charAt(i) && i == j) {
+				temp[j] = "*";
+			}
+			
+			if(user.charAt(j) == word.charAt(i) && i != j) {
+				temp[j] = "#";
+			}
+			
+			
+			
+		}
+			
+	}
+	String tempWord = "";
+	for (int i = 0 ; i < temp.length; i++) {
+		tempWord += temp[i];
+	}
+	return tempWord;
+}
+}
